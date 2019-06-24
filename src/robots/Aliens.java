2be51a3e90aa7b3662600e48cdee34eb639d253b
yaroslava <zavalnaslava@gmail.com>
2019-06-24 16:10:22 +0300

    first: added Math.round to class Aliens

--- src/robots/Aliens.java
+++ src/robots/Aliens.java
@@ -5,7 +5,7 @@ import java.util.Random;
 public class Aliens {
     public static char planetRandom(){
         Random r = new Random();
-        char planetRand = (char) (65 + r.nextInt(3));
+        char planetRand = (char) (Math.round(65 + r.nextInt(3)));
         return planetRand;
     }
 }
