b7d08b8b6f981403aef589824d84c08f74bbcbe1
yaroslava <zavalnaslava@gmail.com>
2019-06-26 11:48:05 +0300

    first: add user input limit

--- src/StartMain.java
+++ src/StartMain.java
@@ -52,58 +52,72 @@ public class StartMain {
             robot.move();
             robot.talk();
             robot.shoot();
-
-
         } else if (answer.equals("b")) {
-            System.out.println("Which kind of domestic robot you would like?\na) Vacuum cleaner \nb) Dishwasher\n");
-            String answer2 = sc.nextLine();
-            if (answer2.equals("a")) {
-                DomesticBot domBot = new DomesticBot(true, false, "vacuum cleaner");
-                System.out.println("Let`s test me!");
-                domBot.formGen();
-                domBot.move();
-                domBot.talk();
-                domBot.clean();
-                domBot.shoot();
-
-            } else if (answer2.equals("b")) {
-                DomesticBot domBot2 = new DomesticBot(true, false, "dishwasher");
-                System.out.println("Let`s test me!");
-                domBot2.formGen();
-                domBot2.move();
-                domBot2.talk();
-                domBot2.clean();
-                domBot2.shoot();
-
+            while(true) {
+                System.out.println("Which kind of domestic robot you would like?\na) Vacuum cleaner \nb) Dishwasher\n");
+                String answer2 = sc.nextLine();
+                if (answer2.equals("a")) {
+                    DomesticBot domBot = new DomesticBot(true, false, "vacuum cleaner");
+                    System.out.println("Let`s test me!");
+                    domBot.formGen();
+                    domBot.move();
+                    domBot.talk();
+                    domBot.clean();
+                    domBot.shoot();
+                    break;
+                } else if (answer2.equals("b")) {
+                    DomesticBot domBot2 = new DomesticBot(true, false, "dishwasher");
+                    System.out.println("Let`s test me!");
+                    domBot2.formGen();
+                    domBot2.move();
+                    domBot2.talk();
+                    domBot2.clean();
+                    domBot2.shoot();
+                    break;
+                }else{
+                    System.out.println("You`ve pressed wrong key! Try again!");
+                    continue;
+                }
             }
         } else if (answer.equals("c")) {
-            System.out.println("This kind of robot can search for some news. Would you like to have \na) good news \nb) bad news\nc)both");
-            String answer3 = sc.nextLine();
-            if (answer3.equals("a")) {
-                AndroidBot android = new AndroidBot(true, false,  true);
-                System.out.println("Let`s test me!");
-                android.formGen();
-                android.move();
-                android.talk();
-                android.searchNews("good news");
-                android.shoot();
-            } else if (answer3.equals("b")) {
-                AndroidBot android2 = new AndroidBot(true, false,  true);
-                System.out.println("Let`s test me!");
-                android2.formGen();
-                android2.move();
-                android2.talk();
-                android2.searchNews("bad news");
-                android2.shoot();
-            } else if (answer3.equals("c")) {
-                AndroidBot android3 = new AndroidBot(true, false,  true);
-                System.out.println("Let`s test me!");
-                android3.formGen();
-                android3.move();
-                android3.talk();
-                android3.searchNews("both");
-                android3.shoot();
+            while(true) {
+                System.out.println("This kind of robot can search for some news. Would you like to have \na) good news \nb) bad news\nc)both");
+                String answer3 = sc.nextLine();
+                if (answer3.equals("a")) {
+                    AndroidBot android = new AndroidBot(true, false, true);
+                    System.out.println("Let`s test me!");
+                    android.formGen();
+                    android.move();
+                    android.talk();
+                    android.searchNews("good news");
+                    android.shoot();
+                    break;
+                } else if (answer3.equals("b")) {
+                    AndroidBot android2 = new AndroidBot(true, false, true);
+                    System.out.println("Let`s test me!");
+                    android2.formGen();
+                    android2.move();
+                    android2.talk();
+                    android2.searchNews("bad news");
+                    android2.shoot();
+                    break;
+                } else if (answer3.equals("c")) {
+                    AndroidBot android3 = new AndroidBot(true, false, true);
+                    System.out.println("Let`s test me!");
+                    android3.formGen();
+                    android3.move();
+                    android3.talk();
+                    android3.searchNews("both");
+                    android3.shoot();
+                    break;
+                }else {
+                    System.out.println("You`ve pressed wrong key! Try again!");
+                    continue;
+                }
             }
+        }else{
+            System.out.println("You`ve pressed wrong key! Try again!");
+            start();
         }
     }
 }
